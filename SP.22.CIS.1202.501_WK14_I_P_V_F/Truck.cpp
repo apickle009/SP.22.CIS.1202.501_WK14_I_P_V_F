0029f44736150c24c9c69b0f8930aed6fef483a1
TonyP009 <apickle009@gmail.com>
2022-04-24 01:33:27 -0400

    Updated Truck.cpp with all data

---
+++ SP.22.CIS.1202.501_WK14_I_P_V_F/Truck.cpp
@@ -0,0 +1,29 @@
+//Tony Pickle
+//SP.22.CIS.1202.501 
+//4.23.22
+
+
+#include <string>
+#include "Truck.h"
+#include <iostream>
+
+
+using namespace std;
+
+int Truck::getTowingCapa() {
+
+	return  Truck::towCap;
+}
+
+void Truck::setTowingCapa(int tC) {
+
+	Truck::towCap = tC;
+
+}
+
+void Truck::displayInfo() {
+
+	Vehicle::displayInfo();
+	cout << "\nTow Capacity: " << towCap;
+
+}
