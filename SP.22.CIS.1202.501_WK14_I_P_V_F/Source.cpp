a62d58d574cb3daee07cf966df34c808cba85572
TonyP009 <apickle009@gmail.com>
2022-04-24 02:13:02 -0400

    Updated Car Class code. Compiled and ran. Some incorrect syntax looks to have casused an issue.

--- SP.22.CIS.1202.501_WK14_I_P_V_F/Source.cpp
+++ SP.22.CIS.1202.501_WK14_I_P_V_F/Source.cpp
@@ -20,19 +20,43 @@ int main() {
 	Car urCar;
 	Truck urTruck; 
 
-	cout << "Welcome Driver! " << endl; 
+	cout << "Welcome Driver! " << endl;
+
+	// Collecting and displaying Vehicle Details now
 
 	cout << "\nPlease enter the make of your Vehicle: "; 
 	getline(cin, vehicleMan);
-	urVehicle.setManufacturer(vehicleMan); 
-
+	
 	cout << "\nNow, Please enter the year your " << vehicleMan << " was built: ";
 	cin.ignore();
 	cin >> year;
 	cout << endl <<  endl; 
+
+	urVehicle.setManufacturer(vehicleMan);
 	urVehicle.setYearBuilt(year);
 
 	urVehicle.displayInfo(); 
+
+	// Collecting and displaying Car Details now
+
+	cout << "\nWelcome back Driver! This time we will need some additional details" << endl;
+
+	cout << "\nPlease enter the make of your Vehicle: ";
+	getline(cin, vehicleMan);
+	
+	cout << "\nNow, Please enter the year your " << vehicleMan << " was built: ";
+	cin.ignore();
+	cin >> year;
+
+	cout << "\nPlease provide the number of doors that your " << vehicleMan << " has: ";
+	cin >> doors;
+	cout << endl << endl;
+
+	urCar.setManufacturer(vehicleMan);
+	urCar.setYearBuilt(year);
+	urCar.setDoors(doors);
+
+	urCar.displayInfo(); 
 
 
 
